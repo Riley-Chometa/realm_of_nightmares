5b39321e463dd0e915dcc72c5a66028767533865
Blaine Barlow <barlow.blaine1@gmail.com>
2021-11-11 16:15:15 -0600

    Added new stamina bar and pick up bar, added robot sprites for store keeper, added idle animation for store keeper.

--- Assets/_Scripts/ItemScripts/WeaponPickup.cs
+++ Assets/_Scripts/ItemScripts/WeaponPickup.cs
@@ -6,42 +6,58 @@ public class WeaponPickup : MonoBehaviour
 {
     
     public GameObject floatingText;
+    public GameObject pressE;
     private GameObject tempText;
+    private GameObject tempE;
     public Transform tm;
     [SerializeField]
     private int[] damages = {40, 60, 80, 100, 120, 150};
 
+    private string printMe;
     private int damage;
     private void Start() {
         if (this.gameObject.tag == "sword1"){
-            damage = damages[0];
+            printMe = damages[0] + " Damage";
         }
         else if (this.gameObject.tag == "sword2"){
-            damage = damages[1];
+            printMe = damages[1] + " Damage";
         }
         else if (this.gameObject.tag == "sword3"){
-            damage = damages[2];
+            printMe = damages[2] + " Damage";
         }
         else if (this.gameObject.tag == "sword4"){
-            damage = damages[3];
+            printMe = damages[3] + " Damage";
         }
         else if (this.gameObject.tag == "sword5"){
-            damage = damages[4];
+            printMe = damages[4] + " Damage";
         }
         else if (this.gameObject.tag == "sword6"){
-            damage = damages[5];
+            printMe = damages[5] + " Damage";
         }
+        else if (this.gameObject.tag == "FireBow"){
+            printMe = "Fire Bow";
+        }
+        else if (this.gameObject.tag == "BasicBow"){
+            printMe = "Basic Bow";
+        }
+        else if (this.gameObject.tag == "FirePotion"){
+            printMe = "Fire Ball Potion!";
+        }
+
     }
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player"){
             tempText = Instantiate(floatingText, tm.position + new Vector3(0, 2, 0), Quaternion.identity, tm);
-            tempText.GetComponent<TextMesh>().text = damage + " Damage";
+            tempText.GetComponent<TextMesh>().text = printMe;
+            tempE = Instantiate(pressE, tm.position + new Vector3(0, -1, 0), Quaternion.identity, tm);
+            tempE.GetComponent<Transform>().localScale = new Vector3(.1f, .1f, .1f);
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         if (other.gameObject.tag == "Player"){
             Destroy(tempText, 0.1f);
+            Destroy(tempE, .01f);
         }
     }
 }
