646a46b2f02c3ef90965db186a506e761a33a28b
chometa-r <chometa5307@saskpolytech.ca>
2021-11-14 11:01:53 -0600

    fixing merge issues --riley

--- Assets/_Scripts/DungeonGeneration/RoomFirstDungeonGenerator.cs
+++ Assets/_Scripts/DungeonGeneration/RoomFirstDungeonGenerator.cs
@@ -85,6 +85,7 @@ public class RoomFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
         foreach (var room in this.dungeon.rooms)
         {
             if (room.roomType == RoomType.Spawn)
+                //Instantiate(this.Player, new Vector3(room.roomBounds.center.x, room.roomBounds.center.y, -1))
                 this.Player.transform.position = new Vector3(room.roomBounds.center.x, room.roomBounds.center.y, -1);
             else if (room.roomType == RoomType.End)
                 this.DungeonEndPoint.transform.position = new Vector3(room.roomBounds.center.x, room.roomBounds.center.y, -1);
